c04a04bc1215424118969efe906c151ebfed7118
MaxLutton <maxlutton@ucla.edu>
2020-05-10 22:48:01 -0700

    Getting JWT token from API

--- java/com/example/plannerappandroid/MainActivity.java
+++ java/com/example/plannerappandroid/MainActivity.java
@@ -4,15 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 
 import com.android.volley.toolbox.JsonObjectRequest;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import org.json.JSONException;
 import org.json.JSONObject;
 
 
@@ -39,10 +44,42 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, String.format("Logging in with credentials: email: %s password: %s",email, password), Toast.LENGTH_SHORT).show();
 
                 //Setup request.
-                String url = apiBaseUrl + "api-auth/login/";
-                Map<String, String> payloadDict = new HashMap<String, String>();
-                payloadDict.put("")
-                JsonObjectRequest loginRequest = new JsonObjectRequest(Request.Method.POST, )
+                String tokenUrl = apiBaseUrl + "token/";
+                String body = String.format("{\"username\":\"%s\", \"password\":\"%s\"}", email, password);
+                JSONObject jsonBody = null;
+                try {
+                    jsonBody = new JSONObject(body);
+                } catch (JSONException e) {
+                    //Toast.makeText(MainActivity.this, "Entered invalid credentials! Require both email and password.", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, body, Toast.LENGTH_LONG).show();
+                    Log.d("Um", body);
+                    e.printStackTrace();
+                    return;
+                }
+                JsonObjectRequest loginRequest = new JsonObjectRequest(Request.Method.POST, tokenUrl, jsonBody, new  Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast.makeText(MainActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
+                        /*
+                        1. Check status code.
+                        2. If failed, then
+                            a) 403: Tell user that user/pwd is wrong
+                            b) Timeout? Tell user need internet access...
+                        3. If pass, then store both tokens in internal memory. Redirect to home activity.
+                         */
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+
+                    }
+                });
+
+                //Add request to queue!
+                VolleyController.getInstance(getApplicationContext()).addToRequestQueue(loginRequest);
             }
         });
 
