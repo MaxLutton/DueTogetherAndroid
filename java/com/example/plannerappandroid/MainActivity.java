eddeeffa2ac4478cfca18adac3545e5b6b2417d1
MaxLutton <maxlutton@ucla.edu>
2020-05-11 22:15:41 -0700

    Saving jwt tokens in SharedPreferences. Also handling login errors.

--- java/com/example/plannerappandroid/MainActivity.java
+++ java/com/example/plannerappandroid/MainActivity.java
@@ -3,13 +3,20 @@ package com.example.plannerappandroid;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NoConnectionError;
 import com.android.volley.Request;
 import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
 import com.android.volley.VolleyError;
 
 import com.android.volley.toolbox.JsonObjectRequest;
@@ -45,35 +52,55 @@ public class MainActivity extends AppCompatActivity {
 
                 //Setup request.
                 String tokenUrl = apiBaseUrl + "token/";
-                String body = String.format("{\"username\":\"%s\", \"password\":\"%s\"}", email, password);
-                JSONObject jsonBody = null;
+                JSONObject body = new JSONObject();
                 try {
-                    jsonBody = new JSONObject(body);
-                } catch (JSONException e) {
-                    //Toast.makeText(MainActivity.this, "Entered invalid credentials! Require both email and password.", Toast.LENGTH_LONG).show();
-                    Toast.makeText(MainActivity.this, body, Toast.LENGTH_LONG).show();
-                    Log.d("Um", body);
+                    body.put("username", email);
+                    body.put("password", password);
+                }catch (JSONException e){
                     e.printStackTrace();
-                    return;
                 }
-                JsonObjectRequest loginRequest = new JsonObjectRequest(Request.Method.POST, tokenUrl, jsonBody, new  Response.Listener<JSONObject>() {
+                JsonObjectRequest loginRequest = new JsonObjectRequest(Request.Method.POST, tokenUrl, body, new  Response.Listener<JSONObject>() {
 
                     @Override
                     public void onResponse(JSONObject response) {
-                        Toast.makeText(MainActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
-                        /*
-                        1. Check status code.
-                        2. If failed, then
-                            a) 403: Tell user that user/pwd is wrong
-                            b) Timeout? Tell user need internet access...
-                        3. If pass, then store both tokens in internal memory. Redirect to home activity.
-                         */
+                        Toast.makeText(MainActivity.this, "Welcome back :)", Toast.LENGTH_LONG).show();
+                        String accessToken;
+                        String refreshToken;
+                        try {
+                            accessToken = response.getString("access");
+                            refreshToken = response.getString("refresh");
+                        } catch(JSONException e){
+                            e.printStackTrace();
+                            Toast.makeText(MainActivity.this, "Unable to parse response :(", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        Context context = getApplicationContext();
+                        SharedPreferences sharedPrefs = context.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPrefs.edit();
+                        editor.putString("access", accessToken);
+                        editor.putString("refresh", refreshToken);
+                        editor.apply();
                     }
                 }, new Response.ErrorListener() {
 
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         // TODO: Handle error
+                        if (error instanceof TimeoutError){
+                            Toast.makeText(MainActivity.this, "Server down Server down!", Toast.LENGTH_LONG).show();
+                        }
+                        else if (error instanceof NoConnectionError){
+                            Toast.makeText(MainActivity.this, "Please ensure wifi or data is enabled.", Toast.LENGTH_SHORT).show();
+                        }
+                        else if (error instanceof AuthFailureError){
+                            Toast.makeText(MainActivity.this, "Invalid credentials.", Toast.LENGTH_SHORT).show();
+                        }
+                        else if (error instanceof ServerError){
+                            Toast.makeText(MainActivity.this, "Server error! No bueno...", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "Other error... Bad stuff man.", Toast.LENGTH_SHORT).show();
+                        }
 
                     }
                 });
